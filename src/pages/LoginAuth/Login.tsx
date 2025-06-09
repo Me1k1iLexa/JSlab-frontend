9b3cfc659f2fd0b9e5e0d02e15dffe8bc12e9d69
Alexey Markevich <justme1k1i@gmail.com>
2025-06-09 17:04:53 +0700

    Feat: add redirect to login from register refactor: rename css file

--- src/pages/LoginAuth/Login.tsx
+++ src/pages/LoginAuth/Login.tsx
@@ -1,17 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import type {RootState} from "../../store";
 import {loginRequest} from "../../store/user/userSlice.ts";
 import styles from '@/styles/pages/loginAuth.module.css'
+import {useNavigate} from "react-router-dom";
 
 const LoginPage = () => {
     const dispatch = useDispatch();
     const loading = useSelector((state: RootState) => state.user.loading);
     const error = useSelector((state: RootState) => state.user.error);
-
+    const navigate = useNavigate();
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
+    useEffect(() => {
+        if (!loading && !error) {
+            navigate('/login');
+        }
+    }, [loading, error, navigate]);
+    
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault();
         dispatch(loginRequest({email, password}));
